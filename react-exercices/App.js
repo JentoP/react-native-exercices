1fa5a5c9cd9e5af2efc3754266d8e97da7d6b0ec
JentoP <jento.pieters@gmail.com>
2025-11-19 21:09:10 +0100

    Refactor App layout with styles and container

--- react-exercices/App.js
+++ react-exercices/App.js
@@ -1,25 +1,25 @@
 import {StatusBar} from 'expo-status-bar';
-import {Platform, View} from 'react-native';
+import {Platform, View, StyleSheet} from 'react-native';
 import {HomeScreen} from "./screens/HomeScreen";
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 
 export function ProvidedApp() {
     return (
-        <>
+        <View style={styles.container}>
             <HomeScreen/>
-        </>
+        </View>
     );
 }
 
 export default function App() {
     return (
         Platform.select({
-            web: <View>
+            web: <View style={styles.container}>
                 <ProvidedApp/>
                 <StatusBar/>
             </View>,
-            default: <View>
+            default: <View style={styles.container}>
                 <SafeAreaView>
                     <ProvidedApp/>
                 </SafeAreaView>
@@ -28,3 +28,22 @@ export default function App() {
         })
     );
 }
+const styles = StyleSheet.create({
+    container: Platform.select({
+        ios: {
+            flex: 1,
+            backgroundColor: '#000',
+            alignItems: 'center',
+        },
+        android: {
+            flex: 1,
+            backgroundColor: '#000',
+            alignItems: 'center',
+        },
+        web: {
+            flex: 1,
+            backgroundColor: '#000',
+            alignItems: 'center',
+        }
+    }),
+});
--- react-exercices/screens/HomeScreen.js
+++ react-exercices/screens/HomeScreen.js
@@ -2,19 +2,24 @@ import {View, Text, StyleSheet} from "react-native";
 
 export function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Home Screen</Text>
+        <View style={styles.bannerView}>
+            <Text style={styles.buttonText}>Home Screen</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#000',
+    bannerView: {
+        backgroundColor: '#370095',
         alignItems: 'center',
         justifyContent: 'center',
+        height: 100,
+        padding: 20,
+        width: 220,
+        borderRadius: 10,
+        margin: 10,
     },
-    text: {
+    buttonText: {
         color: '#fff',
         fontSize: 20,
         fontWeight: 'bold',
